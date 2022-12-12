b8811ce1801833b4129473506f365a5b785eacea
Steve Watson <steve_watson@next.co.uk>
2022-12-12 14:52:58 +0000

    Simple text changes

--- pubmodule.go
+++ pubmodule.go
@@ -1,5 +1,5 @@
 package pubmodule
 
 func Hello() string {
-	return "Hello, you again! just testing refresh of files"
+	return "Hello, you again! just testing refresh of files, some more ok!!!!!!!"
 }
